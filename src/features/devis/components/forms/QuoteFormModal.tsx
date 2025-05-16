30e1098ff929b85e658f917384a39e48d60e457f
joaquimgameiro <joaquim.progmr@gmail.com>
2025-05-16 17:55:26 +0200

    devis position in progress

--- src/features/devis/components/forms/QuoteFormModal.tsx
+++ src/features/devis/components/forms/QuoteFormModal.tsx
@@ -437,7 +437,7 @@ export const QuoteFormModal: React.FC<QuoteFormModalProps> = ({
         )}
 
         {/* Formulaire à gauche */}
-        <div className="w-3/6 bg-gray-50 overflow-y-auto border-r border-gray-200 custom-scrollbar">
+        <div className="w-3/6 bg-gray-50 overflow-y-auto border-r border-gray-200 flex flex-col h-full custom-scrollbar">
           <Form onSubmit={(e) => e.preventDefault()}>
             {showSettings ? (
               <div className="p-6">
